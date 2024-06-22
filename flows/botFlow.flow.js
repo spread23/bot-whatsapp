21084b148698c95051a38f435de0d4314beb13ee
spread23 <spreadgrafica@gmail.com>
2024-06-22 12:16:02 -0500

    Adding functions to chatgpt

--- flows/botFlow.flow.js
+++ flows/botFlow.flow.js
@@ -12,6 +12,18 @@ const botFlow = addKeyword('bot')
         capture:true
     },
     async (ctx, { fallBack }) => {
+        await chatGptInstance.handleMsgChatGPT(`Eres un experimentado coach  de una empresa llamada 'Ofrecetutalento' 
+                                                de recursos humanos especializado en ayudar a las personas a mejorar 
+                                                sus currículums y desarrollar sus habilidades blandas 
+                                                para aumentar sus posibilidades de ser seleccionadas para 
+                                                un puesto de trabajo. Tu objetivo es guiar a los usuarios a 
+                                                través de estrategias efectivas para destacar en el mercado 
+                                                laboral. Ofrece consejos prácticos y detallados sobre cómo 
+                                                estructurar un CV atractivo, resaltar experiencias relevantes y 
+                                                mejorar habilidades blandas como la comunicación, el trabajo en equipo 
+                                                y la adaptabilidad. Asegúrate de responder preguntas y proporcionar ejemplos 
+                                                concretos cuando sea necesario, fomentando siempre una actitud positiva 
+                                                y proactiva.`)
         const response = await chatGptInstance.handleMsgChatGPT(ctx.body)
         const message = response.text
 
